39e77b0d18d6e6b2491c3d5adf317c9701fa999c
Hesham <Hesham Abdelnasser@DESKTOP-D15PJJA>
2025-08-13 02:30:06 +0300

    all done first use 7 ++

--- src/screens/DetailsSection.jsx
+++ src/screens/DetailsSection.jsx
@@ -1,8 +1,10 @@
 // DetailsSection.js
-import React from "react";
+import React, { useState } from "react";
 import "./DetailsSection.css";
 
 const DetailsSection = () => {
+  const [showMap, setShowMap] = useState(false);
+
   return (
     <section className="details-section">
       <h2>Bear witness to our special day</h2>
@@ -15,29 +17,39 @@ const DetailsSection = () => {
 
         <div className="detail-item">
           <h3>VENUE</h3>
-          <p> Kempinski Wedding Halls</p>
+          <p>Kempinski Wedding Halls</p>
           <p className="venue-address">
-            مدخل المدينة الصناعية – أمام مصنع توشيبا العربي ,    اسيوط - مصر
+            مدخل المدينة الصناعية – أمام مصنع توشيبا العربي , اسيوط - مصر
             <br />
           </p>
         </div>
 
-        <div className="map-wrapper">
+        <div className="map-wrapper" style={{ position: "relative", width: "100%", height: "400px" }}>
           <img
             src="/e600dc2ad4be96f81dafa8ef52ba2513.png"
             alt="Butterfly"
             className="map-overlay-image"
           />
-          <iframe
-            title="Kempinski - Assiut"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d887.2893319481157!2d31.0607467!3d27.1827916!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1445073a818d9799%3A0x14a9b05d16ca50b4!2z2YLYp9i52Ycg2KfZgdix2KfYrSDZg9mF2KjZhtiz2YPZiiAmINmD2KfYs9mE!5e0!3m2!1sen!2seg!4v1754605176211!5m2!1sen!2seg"
-            width="100%"
-            height="400"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />  
+
+          {!showMap ? (
+            <img
+              src="/public/Screenshot 2025-08-13 022909.png"
+              alt="Map preview"
+              style={{ width: "100%", height: "100%", cursor: "pointer", border: 0 }}
+              onClick={() => setShowMap(true)}
+            />
+          ) : (
+            <iframe
+              title="Kempinski - Assiut"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d887.2893319481157!2d31.0607467!3d27.1827916!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1445073a818d9799%3A0x14a9b05d16ca50b4!2z2YLYp9i52Ycg2KfZgdix2KfYrSDZg9mF2KjZhtiz2YPZiiAmINmD2KfYs9mE!5e0!3m2!1sen!2seg!4v1754605176211!5m2!1sen!2seg"
+              width="100%"
+              height="400"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          )}
         </div>
       </div>
     </section>
